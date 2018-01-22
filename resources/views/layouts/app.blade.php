f2acde7be204575b16389b92e3f5bdf1623da3a7
drdlee <danielrudi.lee@gmail.com>
2018-01-23 03:30:22 +0800

    ketinggalan, ganti link Route Home. ke admin/home atau {{route('home'}} //// bedasarkan nama yang sudah kita beri ->name('home');

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,7 +75,7 @@
             @if(Auth::check())
               <div class="col-md-4">
                 <ul class="list-group">
-                  <li class="list-group-item"><a href="/home">Home</a></li>
+                  <li class="list-group-item"><a href="{{route('home')}}">Home</a></li>
                   <li class="list-group-item"><a href="{{route('post.create')}}">Create new post</a></li>
                 </ul>
               </div>
